97fa0f874bdc7033f2abd8e2ed27fb15154d56c3
Jeevankarthick B <jeevankarthick232@gmail.com>
2024-01-12 16:51:56 +0530

    initial commit

---
+++ src/features/accounts/BalanceDisplay.js
@@ -0,0 +1,24 @@
+import { connect } from "react-redux";
+
+function formatCurrency(value) {
+  return new Intl.NumberFormat("en", {
+    style: "currency",
+    currency: "USD",
+  }).format(value);
+}
+
+function BalanceDisplay({ balance }) {
+  // redux new way
+  // const balance = useSelector((store) => store.account.balance);
+
+  return <div className="balance">{formatCurrency(balance)}</div>;
+}
+
+function mapStateToProps(state) {
+  return {
+    balance: state.account.balance,
+  };
+}
+
+// redux old way
+export default connect(mapStateToProps)(BalanceDisplay);
